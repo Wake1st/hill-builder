58d70458d1d1c15d0e914291249efb21f45c3e25
Wake1st <joeltwakefield@gmail.com>
2025-02-25 21:06:52 -0700

    improvements

--- README.md
+++ README.md
@@ -8,3 +8,7 @@ A simple game about building hills.
 - [x] issue with ray not selecting mouse over objects
 - [x] store row and col grid indicies
 - [x] every block clicked will affect it's neighbors, and so forth
+- [ ] CLI - for in game debugging and testing
+- [ ] map generator should have custom and proc-gen
+  - [ ] build map and save with CLI
+  - [ ] proc-gen should have ranges per desired results
--- src/map.rs
+++ src/map.rs
@@ -5,8 +5,10 @@ use crate::{block::BlockBundle, mesh::create_cube_mesh};
 const MAP_SIZE: i32 = 22;
 const GAP: f32 = 0.1;
 
-const ROW_AMPLITUDE: f32 = 2.2;
+const ROW_AMPLITUDE: f32 = 2.8;
 const COL_AMPLITUDE: f32 = 1.4;
+const ROW_WAVELENGTH: f32 = 0.2;
+const COL_WAVELENGTH: f32 = 0.1;
 
 const GROUND_COLOR: Color = Color::srgb(0.0, 0.9, 0.1);
 
@@ -23,24 +25,24 @@ fn setup(
     mut materials: ResMut<Assets<StandardMaterial>>,
     mut meshes: ResMut<Assets<Mesh>>,
 ) {
-    let offset: f32 = (MAP_SIZE as f32) * GAP / 2.0;
+    let offset: f32 = (MAP_SIZE as f32) * (1. + GAP) / 2.0;
 
     for j in 0..MAP_SIZE {
         for i in 0..MAP_SIZE {
-            // set a layer based on Fourier Transform
-            let layer: i32 = (ROW_AMPLITUDE * ops::sin(i as f32) + COL_AMPLITUDE * ops::cos(j as f32)) as i32;
+            let layer: i32 = (ROW_AMPLITUDE * ops::sin(i as f32 * ROW_WAVELENGTH)
+                + COL_AMPLITUDE * ops::cos(j as f32 * COL_WAVELENGTH))
+                as i32;
 
             // create and save a handle to the mesh.
             let cube_mesh_handle: Handle<Mesh> = meshes.add(create_cube_mesh());
-        
+
             // render the mesh with the custom texture, and add the marker.
-            commands.spawn(
-                BlockBundle::new(
+            commands.spawn(BlockBundle::new(
                 cube_mesh_handle,
                 &mut materials,
                 GROUND_COLOR,
                 offset,
-                IVec3::new(i, j, layer)
+                IVec3::new(i, j, layer),
             ));
         }
     }
