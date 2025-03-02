27cf1b11bfd014eb62a54b56d998c80f4bbb6a89
Wake1st <joeltwakefield@gmail.com>
2025-03-01 23:41:26 -0700

    can generate, remove, save, and load map data

--- src/map.rs
+++ src/map.rs
@@ -1,6 +1,5 @@
-use std::f32::consts::PI;
-
 use bevy::prelude::*;
+use serde::{Deserialize, Serialize};
 
 use crate::{
     block::{Block, BlockBundle, Neighborhood},
@@ -8,13 +7,8 @@ use crate::{
     selection::{update_block_selection, update_material_on},
 };
 
-const MAP_SIZE: i32 = 44;
+const MAP_SIZE_DEFAULT: i32 = 8;
 const GAP: f32 = 0.1;
-
-const ROW_AMPLITUDE: f32 = 2.8;
-const COL_AMPLITUDE: f32 = 1.4;
-const ROW_WAVELENGTH: f32 = 0.2;
-const COL_WAVELENGTH: f32 = 0.1;
 
 const GROUND_COLOR: Color = Color::srgb(0.0, 0.9, 0.1);
 
@@ -22,90 +16,126 @@ pub struct MapPlugin;
 
 impl Plugin for MapPlugin {
     fn build(&self, app: &mut App) {
-        app.insert_resource(MapGenSettings::default());
-        app.add_systems(Startup, (pre_setup, setup, allocate_neighbors).chain());
+        app.insert_resource(CurrentMapSettings::default());
+
+        app.add_event::<GenerateMap>()
+            .add_event::<ClearMap>();
+        
+        app.add_systems(Update, (clear_map, store_map, (generate_map, allocate_neighbors).chain()));
     }
 }
 
-#[derive(Resource, Default, Debug)]
-pub struct MapGenSettings {
-    pub amplitude: Vec2,
-    pub wavelength: Vec2,
-    pub vertical_shift: Vec2,
-    pub phase_shift: Vec2,
+#[derive(Resource, Debug, Default)]
+pub struct CurrentMapSettings {
+    pub value: MapGenerationSettings,
 }
 
-fn pre_setup(mut map_gen_settings: ResMut<MapGenSettings>) {
-    *map_gen_settings = MapGenSettings {
-        amplitude: Vec2::new(ROW_AMPLITUDE, COL_AMPLITUDE),
-        wavelength: Vec2::new(ROW_WAVELENGTH, COL_WAVELENGTH),
-        vertical_shift: Vec2::ZERO,
-        phase_shift: Vec2::new(-PI, PI / 2.),
-    };
+#[derive(Event)]
+pub struct GenerateMap {
+    pub settings: MapGenerationSettings,
 }
 
-#[derive(Resource, Default, Debug)]
-pub struct MapGenSettings {
-    pub amplitude: Vec2,
-    pub wavelength: Vec2,
-    pub vertical_shift: Vec2,
-    pub phase_shift: Vec2,
+#[derive(Event)]
+pub struct ClearMap;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct MapGenerationSettings {
+    pub size: i32,
+    pub terrain: TerrainSettings,
 }
 
-fn pre_setup(mut map_gen_settings: ResMut<MapGenSettings>) {
-    *map_gen_settings = MapGenSettings {
-        amplitude: Vec2::new(ROW_AMPLITUDE, COL_AMPLITUDE),
-        wavelength: Vec2::new(ROW_WAVELENGTH, COL_WAVELENGTH),
-        vertical_shift: Vec2::ZERO,
-        phase_shift: Vec2::new(-PI, PI / 2.),
-    };
-}
-
-pub enum TerrainType {
-    FLAT,
-    CURVED()
-}
-
-
-fn setup(
-    mut commands: Commands,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-    mut meshes: ResMut<Assets<Mesh>>,
-    map_gen_settings: Res<MapGenSettings>,
-) {
-    let hover_matl = materials.add(Color::WHITE);
-    let ground_matl = materials.add(GROUND_COLOR);
-    let map_offset: f32 = (MAP_SIZE as f32) * (1. + GAP) / 2.0;
-
-    for i in 0..MAP_SIZE {
-        for j in 0..MAP_SIZE {
-            let layer: i32 = generate_layer(i, j, &map_gen_settings);
-
-            // create and save a handle to the mesh.
-            let cube_mesh_handle: Handle<Mesh> = meshes.add(create_cube_mesh());
-
-            // render the mesh with the custom texture, and add the marker.
-            commands
-                .spawn(BlockBundle::new(
-                    cube_mesh_handle,
-                    ground_matl.clone(),
-                    map_offset,
-                    IVec3::new(i, j, layer),
-                ))
-                .observe(update_material_on::<Pointer<Over>>(hover_matl.clone()))
-                .observe(update_material_on::<Pointer<Out>>(ground_matl.clone()))
-                .observe(update_block_selection::<Pointer<Down>>());
+impl Default for MapGenerationSettings {
+    fn default() -> Self {
+        Self { 
+            size: MAP_SIZE_DEFAULT, 
+            terrain: Default::default() 
         }
     }
 }
 
-fn generate_layer(x: i32, y: i32, map_gen_settings: &MapGenSettings) -> i32 {
-    (map_gen_settings.amplitude.x
-        * ops::sin(x as f32 * map_gen_settings.wavelength.x + map_gen_settings.phase_shift.x)
-        + map_gen_settings.vertical_shift.x
-        + map_gen_settings.amplitude.y
-            * ops::cos(y as f32 * map_gen_settings.wavelength.y + map_gen_settings.phase_shift.y)
-        + map_gen_settings.vertical_shift.y) as i32
+#[derive(Debug, Default, Clone, Serialize, Deserialize)]
+pub enum TerrainSettings {
+    #[default]
+    FLAT,
+    CURVED(CurvedTerrainSettings)
+}
+
+#[derive(Resource, Default, Debug, Clone, Serialize, Deserialize)]
+pub struct CurvedTerrainSettings {
+    pub amplitude: Vec2,
+    pub wavelength: Vec2,
+    pub vertical_shift: Vec2,
+    pub phase_shift: Vec2,
+}
+
+fn clear_map(
+    mut event: EventReader<ClearMap>,
+    mut blocks: Query<Entity, With<Block>>,
+    mut commands: Commands,
+) {
+    for _ in event.read() {
+        for entity in blocks.iter_mut() {
+            commands.entity(entity).despawn_recursive();
+        }
+    }
+}
+
+fn store_map(
+    mut event: EventReader<GenerateMap>,
+    mut settings: ResMut<CurrentMapSettings>
+) {
+    for generation in event.read() {
+        settings.value = generation.settings.clone();
+    }
+}
+
+fn generate_map(
+    mut event: EventReader<GenerateMap>,
+    mut commands: Commands,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+) {
+    for generation in event.read() {
+
+        let hover_matl = materials.add(Color::WHITE);
+        let ground_matl = materials.add(GROUND_COLOR);
+        let cube_mesh_handle: Handle<Mesh> = meshes.add(create_cube_mesh());
+        
+        let map_size = generation.settings.size;
+        let map_offset: f32 = (map_size as f32) * (1. + GAP) / 2.0;
+
+        for i in 0..map_size {
+            for j in 0..map_size {
+                let layer: i32 = match &generation.settings.terrain {
+                    TerrainSettings::FLAT => 0,
+                    TerrainSettings::CURVED(settings) => generate_layer(i, j, &settings),
+                };
+
+                // create and save a handle to the mesh.
+
+                // render the mesh with the custom texture, and add the marker.
+                commands
+                    .spawn(BlockBundle::new(
+                        cube_mesh_handle.clone(),
+                        ground_matl.clone(),
+                        map_offset,
+                        IVec3::new(i, j, layer),
+                    ))
+                    .observe(update_material_on::<Pointer<Over>>(hover_matl.clone()))
+                    .observe(update_material_on::<Pointer<Out>>(ground_matl.clone()))
+                    .observe(update_block_selection::<Pointer<Down>>());
+            }
+        }
+    }
+}
+
+fn generate_layer(x: i32, y: i32, settings: &CurvedTerrainSettings) -> i32 {
+    (settings.amplitude.x
+        * ops::sin(x as f32 * settings.wavelength.x + settings.phase_shift.x)
+        + settings.vertical_shift.x
+        + settings.amplitude.y
+            * ops::cos(y as f32 * settings.wavelength.y + settings.phase_shift.y)
+        + settings.vertical_shift.y) as i32
 }
 
 fn allocate_neighbors(
