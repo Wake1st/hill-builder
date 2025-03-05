989ee7cd3ba79db51f44121102643ce38511f7b6
Wake1st <joeltwakefield@gmail.com>
2025-03-05 15:03:25 -0700

    meh, good message, not working

--- src/instructions.rs
+++ src/instructions.rs
@@ -1,16 +1,20 @@
 use bevy::prelude::*;
 
+use crate::water::{toggle_water, WaterToggle, FILL_KEY};
+
 pub struct InstructionsPlugin;
 
 impl Plugin for InstructionsPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, setup);
+        app.add_systems(Startup, setup)
+            .add_systems(Update, toggle_water_display.after(toggle_water));
     }
 }
 
-fn setup(
-    mut commands: Commands,
-) {
+#[derive(Component)]
+struct WaterToggleText;
+
+fn setup(mut commands: Commands, water_toggle: Res<WaterToggle>) {
     // Text to describe the controls.
     commands.spawn((
         Text::new("Left click a block to pull it up; right click a block to push it down."),
@@ -21,4 +25,28 @@ fn setup(
             ..default()
         },
     ));
+
+    // Text to describe the water toggle
+    commands.spawn((
+        Text::new(format!("Water toggled: {:?}", water_toggle.0)),
+        WaterToggleText,
+        Node {
+            position_type: PositionType::Absolute,
+            top: Val::Px(36.0),
+            left: Val::Px(12.0),
+            ..default()
+        },
+    ));
+}
+
+fn toggle_water_display(
+    keys: Res<ButtonInput<KeyCode>>,
+    toggle: ResMut<WaterToggle>,
+    mut query: Query<&mut TextSpan, With<WaterToggleText>>,
+) {
+    if keys.just_pressed(FILL_KEY) {
+        if let Ok(mut text) = query.get_single_mut() {
+            **text = format!("Water toggled: {:?}", toggle.0);
+        };
+    }
 }
